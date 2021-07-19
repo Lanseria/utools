579aa1648765c067e02e7ae4c54e9ae1e1f09a3c
Lanseria <zhangchao564265135@hotmail.com>
2021-07-19 15:41:31 +0800

    first add treeForeach

--- src/tree/index.ts
+++ src/tree/index.ts
@@ -1,5 +1,24 @@
-const treeForeach = () => {
-  console.log("treeForeach");
+const dfs = (node: IObj, cb?: Fn) => {
+  // console.log(node);
+  if (cb) {
+    cb(node);
+  }
+  if (node.children && node.children.length) {
+    for (let i = 0; i < node.children.length; i++) {
+      const m = node.children[i];
+      dfs(m, cb);
+    }
+  }
 };
 
-export default treeForeach;
+/**
+ *
+ * @param node A Node width children
+ * @param cb A Function
+ * @returns void
+ */
+const treeForeach = (node: IObj, cb: Fn) => {
+  dfs(node, cb);
+};
+
+export { treeForeach };
---
+++ src/types/index.d.ts
@@ -0,0 +1,12 @@
+interface Fn<T = any, R = T> {
+  (...arg: T[]): R;
+}
+
+interface PromiseFn<T = any, R = T> {
+  (...arg: T[]): Promise<R>;
+}
+
+interface IObj<T = any> {
+  [key: string]: T;
+  [key: number]: T;
+}
