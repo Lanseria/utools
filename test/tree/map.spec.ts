4cec7252693d0623f6325fa8b9d6301d51b3c558
Lanseria <zhangchao564265135@hotmail.com>
2021-07-21 14:08:03 +0800

    add treeClone

--- test/tree/map.spec.ts
+++ test/tree/map.spec.ts
@@ -1,12 +1,18 @@
 import { treeMap } from "@lanseria/utools";
+import { isEqualWith } from "lodash-es";
 import { data1 } from "./mock";
 
 describe("Tree Map 树循环操作", () => {
-  it("添加属性", () => {
+  it("更换属性名", () => {
     const newData = treeMap(data1[0], (item: INode) => {
       item.id = item.name;
       delete item.name;
     });
-    console.log(JSON.stringify(newData));
+    const res = isEqualWith(data1[0], newData, (objValue, othValue) => {
+      if (objValue.name == othValue.id) {
+        return true;
+      }
+    });
+    expect(res).toBe(true);
   });
 });
